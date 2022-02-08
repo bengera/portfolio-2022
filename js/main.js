6763346f61947b0a190bcb21f183193b0cb1f4bc
unknown <bengera89@gmail.com>
2022-02-08 14:14:05 +0700

    work-display-update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const menuBtn = document.querySelector('.menu-btn')
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if (!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  }
-});
+function scrollWork() {
+    let workSection = document.querySelector(".work");
+    workSection.scrollIntoView(true);
+
+    
+}
+
+function scrollAbout() {
+    let aboutSection = document.querySelector(".about");
+    aboutSection.scrollIntoView(true);
+
+    
+}
