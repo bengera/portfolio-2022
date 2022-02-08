dc41763fd0903e0a189dbf28e3173dcd1313e182
unknown <bengera89@gmail.com>
2022-02-08 18:11:37 +0700

    first-animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const h1 = document.querySelector('h1');
+const tagLine = document.querySelector('.hero__tagline');
+const cta = document.querySelector('.hero__cta');
+
+
+window.addEventListener('load', () => {
+    console.log('loaded');
+    h1.classList.add('show')
+})
+
+
 function scrollWork() {
     let workSection = document.querySelector(".work");
     workSection.scrollIntoView(true);
